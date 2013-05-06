2ac378b33413784547542fdc2eea38d566e2f48a
mike <mssteven@student.umass.edu>
2013-05-05 20:21:54 -0400

    GameEngine update
    initial state and design implemented

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,6 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
@@ -9,154 +6,154 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+
+/**
+ * Top down shooter, of zombies.
+ *
+ * @author Mike Stevens & Kevin Rizza
+ */
 public class GameEngine {
 
-	FileReader fr;
-	BufferedReader br;
-	
-	
-	
-	
-	/** position of quad */
-	float x = 400, y = 300;
-	/** angle of quad rotation */
-	float rotation = 0;
-	
-	/** time at last frame */
-	long lastFrame;
-	
-	/** frames per second */
-	int fps;
-	/** last fps time */
-	long lastFPS;
+    private static final String WINDOW_TITLE = "ZombiesAteMyHomework";
+    private static final int[] WINDOW_DIMENSIONS = {800, 600};
+    long lastFrame;//time at last frame        
+	float x = 400, y = 300;//position of player	
+	int fps;//frames per second
+	long lastFPS;//fps at last frame
+    
+    
+    private void render() {
+    	// Clear The Screen And The Depth Buffer
+    			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
-	public void start() throws FileNotFoundException {
-		try {
-			Display.setDisplayMode(new DisplayMode(800, 600));
-			Display.create();
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
+    			// R,G,B,A Set The Color To Blue One Time Only
+    			GL11.glColor3f(0.5f, 0.5f, 1.0f);
 
-		initGL(); // init OpenGL
-		getDelta(); // call once before loop to initialise lastFrame
-		lastFPS = getTime(); // call before loop to initialise fps timer
+    			// draw player
+    			GL11.glPushMatrix();
+    				GL11.glTranslatef(x, y, 0);
+    				GL11.glTranslatef(-x, -y, 0);    				
+    				GL11.glBegin(GL11.GL_QUADS);
+    					GL11.glVertex2f(x - 50, y - 50);
+    					GL11.glVertex2f(x + 50, y - 50);
+    					GL11.glVertex2f(x + 50, y + 50);
+    					GL11.glVertex2f(x - 50, y + 50);
+    				GL11.glEnd();
+    			GL11.glPopMatrix();  
+    }
 
-		while (!Display.isCloseRequested()) {
-			int delta = getDelta();
-			
-			update(delta);
-			renderGL();
+    private static void logic() {
+        // Add logic code here
+    }
 
-			Display.update();
-			Display.sync(60); // cap fps to 60fps
-		}
-
-		Display.destroy();
-	}
-	
-	public void update(int delta) {
-		// rotate quad
-		rotation += 0.15f * delta;
-		
-		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) x -= 0.35f * delta;
+    private void input(int delta) {
+       
+    	if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) x -= 0.35f * delta;
 		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) x += 0.35f * delta;
 		
 		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) y += 0.35f * delta;
 		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) y -= 0.35f * delta;
 		
-		// keep quad on the screen
 		if (x < 0) x = 0;
 		if (x > 800) x = 800;
 		if (y < 0) y = 0;
-		if (y > 600) y = 600;
+		if (y > 600) y = 600;    	
+    }
+
+    private static void cleanUp(boolean asCrash) {
+        // Add cleaning code here
+        Display.destroy();
+        System.exit(asCrash ? 1 : 0);
+    }
+
+    private static void setUpMatrices() {
+        // Add code for the initialization of the projection matrix here
+    }
+
+    private static void setUpStates() {    	
+    
+        //        glEnable(GL_DEPTH_TEST);
+        //        glEnable(GL_LIGHTING);
+        //        glEnable(GL_BLEND);
+        //        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    }
+
+    private void update() {
+    	updateFPS(); // update FPS Counter
+        Display.update();
+        Display.sync(60);    
+        
+    }
+
+    private void enterGameLoop() {
+    	
+    	initGL(); // init OpenGL
+		getTimeElapsed(); // call once before loop to initialise lastFrame
+		lastFPS = getTime(); // call before loop to initialise fps timer
 		
-		updateFPS(); // update FPS Counter
-	}
-	
-	/** 
-	 * Calculate how many milliseconds have passed 
-	 * since last frame.
-	 * 
-	 * @return milliseconds passed since last frame 
-	 */
-	public int getDelta() {
-	    long time = getTime();
-	    int delta = (int) (time - lastFrame);
-	    lastFrame = time;
-	 
-	    return delta;
-	}
-	
-	/**
-	 * Get the accurate system time
-	 * 
-	 * @return The system time in milliseconds
-	 */
-	public long getTime() {
-	    return (Sys.getTime() * 1000) / Sys.getTimerResolution();
-	}
-	
-	/**
-	 * Calculate the FPS and set it in the title bar
-	 */
-	public void updateFPS() {
+        while (!Display.isCloseRequested()) {
+        	int delta = getTimeElapsed();    	
+        	logic();
+        	render();
+            input(delta);
+            update();
+          
+        }
+    }
+
+    private static void setUpDisplay() {
+        try {
+            Display.setDisplayMode(new DisplayMode(WINDOW_DIMENSIONS[0], WINDOW_DIMENSIONS[1]));
+            Display.setTitle(WINDOW_TITLE);
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            cleanUp(true);
+        }
+    }
+    
+    
+    
+    public void updateFPS() {
 		if (getTime() - lastFPS > 1000) {
 			Display.setTitle("FPS: " + fps);
 			fps = 0;
 			lastFPS += 1000;
 		}
 		fps++;
-	}
-	
+	}    
 	public void initGL() {
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
 		GL11.glOrtho(0, 800, 0, 600, 1, -1);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
-		
-		
-		
-		
 	}
-
-	public void renderGL() throws FileNotFoundException {
-		
-
-		
-		
-		// Clear The Screen And The Depth Buffer
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-
-		// R,G,B,A Set The Color To Blue One Time Only
-		GL11.glColor3f(0.5f, 0.5f, 1.0f);
-
-		// draw quad
-		GL11.glPushMatrix();
-			GL11.glTranslatef(x, y, 0);
-			//GL11.glRotatef(rotation, 0f, 0f, 1f);
-			GL11.glTranslatef(-x, -y, 0);
-			
-			
-			
-			
-			
-			GL11.glBegin(GL11.GL_QUADS);
-				GL11.glVertex2f(x - 50, y - 50);
-				GL11.glVertex2f(x + 50, y - 50);
-				GL11.glVertex2f(x + 50, y + 50);
-				GL11.glVertex2f(x - 50, y + 50);
-			GL11.glEnd();
-			
-		GL11.glPopMatrix();
+    
+    public long getTime() {
+	    return (Sys.getTime() * 1000) / Sys.getTimerResolution();
 	}
-	
-	public static void main(String[] argv) throws FileNotFoundException {
-		GameEngine example = new GameEngine();
-		
-		
-		
-		example.start();
+    
+    public int getTimeElapsed() {
+	    long time = getTime();
+	    int delta = (int) (time - lastFrame);
+	    lastFrame = time;
+	 
+	    return delta;
 	}
+    
+    
+    
+    
+    
+
+    public static void main(String[] args) {
+        setUpDisplay();
+        setUpStates();
+        setUpMatrices();
+        GameEngine engine=new GameEngine();
+        engine.enterGameLoop();
+        cleanUp(false);
+    }
 }
