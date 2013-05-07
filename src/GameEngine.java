bde0222f5992e369bca9a68dd70c0e7ee1e562f2
mike <mssteven@student.umass.edu>
2013-05-06 20:40:16 -0400

    awasd

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -62,21 +62,7 @@ public class GameEngine {
 
 	
 	
-	// Moving variables
-		private int projectionMatrixLocation = 0;
-		private int viewMatrixLocation = 0;
-		private int modelMatrixLocation = 0;
-		private Matrix4f projectionMatrix = null;
-		private Matrix4f viewMatrix = null;
-		private Matrix4f modelMatrix = null;
-		private Vector3f modelPos = null;
-		private Vector3f modelAngle = null;
-		private Vector3f modelScale = null;
-		private Vector3f cameraPos = null;
-		private FloatBuffer matrix44Buffer = null;
-	
-	
-	
+
 	
     private void render() {
     	
@@ -218,30 +204,7 @@ public class GameEngine {
     
     private void setUpMatrices() {
     	// Setup projection matrix
-    			projectionMatrix = new Matrix4f();
-    			float fieldOfView = 60f;
-    			float aspectRatio = (float)WINDOW_DIMENSIONS[0] / (float)WINDOW_DIMENSIONS[1];
-    			float near_plane = 0.1f;
-    			float far_plane = 100f;
     			
-    			float y_scale = this.coTangent(this.degreesToRadians(fieldOfView / 2f));
-    			float x_scale = y_scale / aspectRatio;
-    			float frustum_length = far_plane - near_plane;
-    			
-    			projectionMatrix.m00 = x_scale;
-    			projectionMatrix.m11 = y_scale;
-    			projectionMatrix.m22 = -((far_plane + near_plane) / frustum_length);
-    			projectionMatrix.m23 = -1;
-    			projectionMatrix.m32 = -((2 * near_plane * far_plane) / frustum_length);
-    			
-    			// Setup view matrix
-    			viewMatrix = new Matrix4f();
-    			
-    			// Setup model matrix
-    			modelMatrix = new Matrix4f();
-    			
-    			// Create a FloatBuffer with the proper size to store our matrices later
-    			matrix44Buffer = BufferUtils.createFloatBuffer(16);
     }
 
     private static void setUpStates() {    
